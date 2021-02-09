5ec1eb4972c640b2b5295946e1c4b6c64ee45cf6
Syed Ali Uz Zaman <45460148+alyaxi@users.noreply.github.com>
2021-02-09 06:10:23 +0500

    Added Diary Compoenet

---
+++ src/features/diary/Diary.tsx
@@ -0,0 +1,97 @@
+import React, {FC, useEffect} from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "../../RootReducer";
+import http from "../../services/api";
+import {Diary} from "../../Interfaces/Diary";
+import {addDiary} from "../diary/diarySlice";
+import Swal from "sweetalert2";
+import {setUser} from "../auth/userSlice";
+import DiaryTile from "../diary/DiaryTil";
+import { User } from "../../Interfaces/User";
+import {Route, Switch} from "react-router-dom";
+import DiaryEntryList from "./DiaryEntryList";
+import {useAppDispatch} from "../../store";
+import dayjs from "dayjs";
+
+const Diaries : FC = () => {
+    const dispatch = useAppDispatch();
+    const diary = useSelector((state: RootState) => state.diary)
+    const user = useSelector((state: RootState) => state.user);
+
+    useEffect(() => {
+        const fetchDiary = async () => {
+            if(user) {
+                http.get<null, Diary[]>(`diary/${user.id}`).then((data) => {
+                    if (data && data.length > 0){
+                        const sortedbyUpdatedAt = data.sort((a,b) => {
+                            return dayjs(b.updatedAt).unix() - dayjs(a.updatedAt).unix()
+                        });
+                        dispatch(addDiary(sortedbyUpdatedAt))
+                    }
+                })
+            }
+        }
+        fetchDiary();       
+    }, [dispatch, user]);
+
+    const createDiary  = async () => {
+        const Result = (await Swal.mixin({
+            input: "text",
+            confirmButtonText: "Next >",
+            showCancelButton: true,
+            progressSteps: ["1" , "2"],
+        }).queue([
+            {
+                titleText: "Diary Title",
+                input: "text"
+
+            },
+            {
+                titleText: "Private or Public Diary",
+                input: "radio",
+                inputOptions: {
+                    private: "Private",
+                    public: "Public",
+                },
+                inputValue: "private"
+            },
+        ])) as any;
+
+        if( Result.value ){
+            const {value} = Result;
+            const {diary, user: _user} = await http.post<Partial<Diary>, {diary: Diary; user: User}>(`diary/`, {
+                title: value[0],
+                type: value[1],
+                userId: user?.id,
+            });
+            if (diary && user){
+                dispatch(addDiary([diary] as Diary[]));
+                dispatch(addDiary([diary] as Diary[]));
+                dispatch(setUser(_user));
+                return Swal.fire({
+                    titleText: "All Done",
+                    confirmButtonText: "OK",
+                })
+            }
+        }
+        Swal.fire({
+            titleText: "Cancelled",
+        })
+    }
+    return (
+        <div style={{padding: "1em 0.4em"}}>
+            <Switch>
+                <Route path = "/diary/:id">
+                <DiaryEntryList/>
+                </Route>
+                <Route path="/">
+                <button onClick={createDiary}>Create New Diary</button>
+                {diary.map((diary, index) => (
+                    <DiaryTile key={index} diary= {diary}/>
+                ))}
+                </Route>
+            </Switch>
+        </div>
+    )
+}
+export default Diaries
--- src/features/diary/DiaryEntryList.tsx
+++ src/features/diary/DiaryEntryList.tsx
@@ -1,4 +1,52 @@
-import React, {FC, useState} from "react";
+import React, {FC, useEffect, useState} from "react";
 import {useParams, Link} from "react-router-dom";
 import {useSelector} from "react-redux";
 import { RootState } from "../../RootReducer";
+import http from "../../services/api";
+import {Entry} from "../../Interfaces/Entry"
+import {setEntry} from "../entry/entrySlice";
+import { setCanEdit, setCurrentlyEdit} from "../entry/editiorSlice";
+import dayjs from "dayjs"
+import {useAppDispatch} from "../../store";
+
+
+const DiaryEntryList: FC = () => {
+    const { entry } = useSelector((state: RootState) => state);
+    const dispatch = useAppDispatch();
+    const { id } = useParams<Entry>();
+
+    useEffect(() => {
+        if (id != null){
+            http
+            .get<null, {entry: Entry[];}>(`/diary/entry/${id}`)
+            .then(({entry: _entry}) => {
+                if(_entry){
+                    const sortbyLastUpdated = _entry.sort((a,b)=> {
+                        return dayjs(b.updatedAt).unix() - dayjs(a.updatedAt).unix()
+                    });
+                    dispatch(setEntry(sortbyLastUpdated))
+                }
+            })
+        }
+    
+    },[id, dispatch])
+
+    return(
+        <div className="entries">
+            <header>
+                <Link to="/">Go back</Link>
+            </header>
+            <ul>
+                {entry.map((entry) => (
+                    <li key={entry.id} onClick={() => {
+                        dispatch(setCurrentlyEdit(entry))
+                        dispatch(setCanEdit(true))
+                    }}></li>
+                    ))}
+            </ul>
+        </div>
+    )
+
+}
+
+export default DiaryEntryList
